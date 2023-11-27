fe3f28b5f84cdadc9c5887b25c1cc4724202e03e
RosieOh <20172207@gm.hannam.ac.kr>
2023-11-27 14:15:24 +0900

    🦁feat : board 컨트롤러 완🦁

--- src/main/java/com/pro06/controller/BoardController.java
+++ src/main/java/com/pro06/controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.pro06.controller;
 
 import com.pro06.domain.BoardDTO;
+import com.pro06.domain.PageRequestDTO;
 import com.pro06.service.BoardService;
 import com.pro06.service.BoardServiceImpl;
 import jakarta.validation.Valid;
@@ -22,18 +23,21 @@ public class BoardController {
 
     private final BoardService boardService;
 
+    // 게시판 리스트 업
     @GetMapping("/list")
-    public String List(Model model) {
+    public String boardList(Model model) {
         return "board/list";
     }
 
+    // 게시판 등록 폼
     @GetMapping("/register")
-    public String registerGET() {
+    public String boardRegisterGET() {
         return "board/register";
     }
 
+    // 게시판 등록
     @PostMapping("/register")
-    public String registerPOST(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+    public String boardRegisterPOST(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         log.info("======== Board Register Start =========");
         if(bindingResult.hasErrors()) {
             log.info("=====에러 대비=======");
@@ -46,15 +50,42 @@ public class BoardController {
         return "redirect:/board/list";
     }
 
+    // 게시판 추가 폼
     @GetMapping("/modify")
-    public String modifyForm(Long bno, Model model) {
+    public String modifyForm(Long bno, PageRequestDTO pageRequestDTO, Model model) {
         BoardDTO boardDTO = boardService.readOne(bno);
+        log.info(boardDTO);
         model.addAttribute("boardDTO", boardDTO);
         return "board/read";
     }
 
+    // 추가
     @PostMapping("/modify")
-    public String modify(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
-        BoardDTO
+    public String boardModify(@Valid BoardDTO boardDTO, PageRequestDTO pageRequestDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        log.info("======== Board modify Start =========");
+
+        if(bindingResult.hasErrors()) {
+            log.info("=====에러 대비=======");
+            String link = pageRequestDTO.getLink();
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            redirectAttributes.addAttribute("bno", boardDTO.getBno());
+            return "redirect:/board/modify?"+link;
+        }
+
+        boardService.modify(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", "modified");
+        redirectAttributes.addAttribute("bno", boardDTO.getBno());
+        return "redirect:/board/read";
+    }
+
+
+    // 삭제
+    @PostMapping("/remove")
+    public String boardRemove(Long bno, RedirectAttributes redirectAttributes) {
+        log.info("======== Board remove Start =========");
+        boardService.remove(bno);
+        redirectAttributes.addFlashAttribute("result", "removed");
+        return "redirect:/board/list";
     }
 }
