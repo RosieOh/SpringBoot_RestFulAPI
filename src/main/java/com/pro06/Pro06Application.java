e8bafb2d21d882504e2f26242e6667d4e62c80cd
RosieOh <20172207@gm.hannam.ac.kr>
2023-11-28 09:13:52 +0900

    🦁fix : SecurityConfig 오류나서 일단 내장으로 돌림🦁

--- src/main/java/com/pro06/Pro06Application.java
+++ src/main/java/com/pro06/Pro06Application.java
@@ -2,8 +2,11 @@ package com.pro06;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// @SpringBootApplication
+// 아래는 스프링 시큐리티 꺼주는거
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Pro06Application {
 
     public static void main(String[] args) {
--- src/main/java/com/pro06/entity/MemberRole.java
+++
@@ -1,5 +0,0 @@
-package com.pro06.entity;
-//사용자 Member에 대한 Role 객체
-public enum MemberRole {
-    USER, ADMIN;
-}
